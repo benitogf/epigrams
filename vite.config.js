2f2a0d38190df25be91c3ba2ad74035a1583f2ce
root <root@hades.localdomain>
2026-01-16 20:57:58 +0800

    add pwa worker

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      filename: 'service.worker.js',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,gif,svg,eot,ttf,woff,woff2}'],
+        navigateFallback: 'index.html',
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/benitogf\.github\.io\/epigrams\/.*/i,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'epigrams-cache',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 60 * 60 * 24 * 30,
+              },
+            },
+          },
+        ],
+      },
+      manifest: false,
+    }),
+  ],
   base: './',
   resolve: {
     alias: {
